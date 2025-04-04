2a0e7814b6860adbc20e3a6750726bf52f2fcde2
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-04 23:29:19 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
-import styled, {keyframes} from "styled-components";
+import styled from "styled-components";
 
-const rotationAnimation = keyframes`
-  0% {
-    transform : rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform : rotate(360deg);
-    border-radius: 0px;
-  }
+
+
+const Title = styled.h1`
+  color : ${(props) => props.theme.textColor};
+
 `;
-
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
-  justify-content : center;
-  align-items: center;
-`;
-
-const Emoji = styled.span`
-  font-size: 50px; 
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: blueviolet;
-  display: flex;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-
-  ${Emoji} {
-    &:hover {
-      font-size: 90px;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor}
 `;
+
+
 
 
 
 function App(){
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😘</Emoji>
-      </Box>
-      <Emoji>💥</Emoji>
+      <Title>안녕하세요.</Title>
     </Wrapper>
   );
 }
