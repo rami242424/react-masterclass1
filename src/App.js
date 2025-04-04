8c38df124c0450922c52a28e9eb7ac02322436ae
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-04 21:45:42 +0900

    2.3 'As'로 속성바꾸기

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Button = styled.button`
+  color: white;
+  background-color: blueviolet;
+  border: 0;
+  border-radius: 15px;
+
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
 
 
 function App(){
   return (
     <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="blue"/>
+      <Button>log-in</Button>
+      <Button as="a" href="/">log-out</Button>
     </Father>
   );
 }
