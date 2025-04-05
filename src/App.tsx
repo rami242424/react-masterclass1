1ac7982be22aabcb644d8cb7c3d8b0c54f7f0bcd
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-05 23:00:21 +0900

    3.2 setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,11 @@ import styled from "styled-components";
 
 
 
-const Title = styled.h1`
-  color : ${(props) => props.theme.textColor};
-
-`;
-const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  width: 100vw;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.backgroundColor}
-`;
-
-
-
-
 
 function App(){
   return (
-    <Wrapper>
-      <Title>안녕하세요.</Title>
-    </Wrapper>
-  );
+    <div></div>
+  )
 }
 
 export default App;
