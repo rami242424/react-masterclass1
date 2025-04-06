3fd3e933ce477c72e5a30fcb12dd05435b5b82bf
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-07 01:37:50 +0900

    OutletContext

--- src/screen/users/Followers.tsx
+++ src/screen/users/Followers.tsx
@@ -1,5 +1,13 @@
+import { useOutletContext } from "react-router-dom";
+
+interface IFollowersContext {
+    nameOfMyUser: string
+}
 function Followers(){
-    return <h1>Followers~</h1>;
+    // const ctx = useOutletContext();
+    // console.log(ctx, "씨티엑스");
+    const { nameOfMyUser } = useOutletContext<IFollowersContext>();
+    return <h1>here are {nameOfMyUser} 의 followers</h1>;
 }
 
 export default Followers;
--- src/screen/users/User.tsx
+++ src/screen/users/User.tsx
@@ -10,7 +10,11 @@ function User(){
             <h1>User with ID {userId} is named: {users[Number(userId)-1].name}</h1>
             <hr />
             <Link to={"followers"}>See followers!</Link>
-            <Outlet />
+            <Outlet
+                context={{
+                    nameOfMyUser: users[Number(userId) - 1].name,
+                }}
+            />
         </div>
     );
 }
