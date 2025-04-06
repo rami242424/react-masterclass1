93b19899122dbf8c45f6eb452126cd9effc2da6e
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-06 23:01:21 +0900

    4.1 BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import styled from "styled-components";
-
-
-
 
 function App(){
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
