54fd07cf009486d3f3fddd9c9b2c5043e9c826cd
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-07 21:14:29 +0900

    5.5 Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,44 @@
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import { Container, Header, Loader, Title } from "./Coins";
+import { useEffect, useState } from "react";
 
 interface RouteParams {
     coinId : string,
 }
+
+interface RouteState {
+    name: string;
+}
 function Coin(){
+    const [loading, setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
-    
-    return <h1>Coin : coinId is {coinId} </h1>
+    const {state} = useLocation<RouteState>();
+    const [info, setInfo] = useState({});
+    const [priceInfo, setPriceInfo] = useState({});
+    useEffect(() => {
+        (async () => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
+        })();
+    }, []);
+    return (
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading.."}</Title>
+            </Header>
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : null
+            }
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div`
+export const Container = styled.div`
     padding: 0px 20px;
     max-width: 480px;
     margin: 0 auto;
 `;
 
-const Header = styled.header`
+export const Header = styled.header`
     height : 10vh;
     display: flex;
     justify-content: center;
@@ -23,9 +23,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom : 10px;
     a {
+        display : flex;
+        align-items: center;
         padding: 20px;
         transition : color 0.2s ease-in;
-        display : block;
     }
     &:hover {
         a {
@@ -35,14 +36,20 @@ const Coin = styled.li`
 `;
 
 
-const Title = styled.h1`
+export const Title = styled.h1`
     font-size: 48px;
     color : ${(props)=> props.theme.accentColor}
 `;
 
-const Loader = styled.span`
+export const Loader = styled.span`
     text-align: center;
     display: block;
+`;
+
+const Img = styled.img`
+    width : 35px;
+    height: 35px;
+    margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -64,7 +71,7 @@ function Coins(){
             const response = await fetch("https://api.coinpaprika.com/v1/coins");
             const json = await response.json();
             setCoins(json.slice(0, 100));
-            // setLoading(false);
+            setLoading(false);
         })();
     }, []);
     // console.log(coins);
@@ -79,7 +86,16 @@ function Coins(){
                     <CoinsList>
                     {coins.map((coin) => (
                         <Coin key={coin.id}>
-                            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                            <Link 
+                                to={{
+                                    pathname: `/${coin.id}`,
+                                    state: {name: coin.name}
+                                }}
+                            >
+                                <Img src={`https://cryptoicon-api.pages.dev/icons/128/color/${coin.symbol.toLowerCase()}.png`} />
+                                {coin.name} &rarr;
+                            </Link> 
+                            
                         </Coin>
                     ))}
                     </CoinsList>
