2fce3ff0923f8c84becb22e540186e080a40d5e1
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-08 20:49:14 +0900

    6.11 Add To Do

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import ToDoList from "./ToDoList";
+import ToDoList from "./components/ToDoList";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
@@ -57,7 +57,8 @@ body {
   font-weight: 300;
   font-family: 'Source Sans Pro', sans-serif;
   background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor};
+  //color:${(props) => props.theme.textColor};
+  color: white;
   line-height: 1.2;
 }
 a {
