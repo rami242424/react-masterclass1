204c14cfc201ef0c79a59e6fe74c988d40a3d7ec
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-09 14:44:07 +0900

    #7.7 Performance : memo 사용 불필요한 리렌더링 하지않기

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import {DragDropContext, Draggable, Droppable, DropResult, } from "react-beautiful-dnd";
+import {DragDropContext, Droppable, DropResult, } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./Components/DraggableCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -27,12 +28,6 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  padding: 10px 10px;
-  margin-bottom: 5px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
 
 
 function App(){
@@ -47,16 +42,16 @@ function App(){
     setToDos(oldToDos => {
       const toDosCopy = [...oldToDos];
       // 1) Delete item on source.index (움직인 item 정보) : 원래자리에서 제거
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
+      //console.log("Delete item on", source.index);
+      //console.log(toDosCopy);
       toDosCopy.splice(source.index, 1);
-      console.log("Deleted item");
-      console.log(toDosCopy);
+      //console.log("Deleted item");
+      //console.log(toDosCopy);
 
       // 2) Put back the item on the destination.index (이동한 위치): 새 위치에 삽입
-      console.log("Put back", draggableId, "on ", destination.index);
+      //console.log("Put back", draggableId, "on ", destination.index);
       toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
+      //console.log(toDosCopy);
       return toDosCopy;
     });
   };
@@ -69,17 +64,7 @@ function App(){
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                   {toDos.map((toDo, index) => (
-                    <Draggable key={toDo} draggableId={toDo} index={index}>
-                    {(magic) => (
-                      <Card 
-                        ref={magic.innerRef}
-                        {...magic.draggableProps}
-                        {...magic.dragHandleProps}
-                      >
-                        {toDo}
-                      </Card>
-                      )}
-                    </Draggable>
+                    <DraggableCard key={toDo} index={index} toDo={toDo} />
                   ))}
                   {magic.placeholder}
               </Board>
