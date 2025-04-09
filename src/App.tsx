f7f96a4190d046056e1aa5cdea4a0fd325dbf811
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-09 23:08:14 +0900

    #8.5 Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,19 @@ const Box = styled(motion.div)`
 
 
 const boxVariants = {
-  hover: {scale: 1.5, rotateZ : 90},
-  click: { scale: 1 , borderRadius: "100px"},
+  hover: { scale: 1.5, rotateZ: 90 },
+  click: { scale: 1, borderRadius: "100px" },
+  drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 }
 
 function App() {
   return (
     <Wrapper>
       <Box 
+        drag
         variants={boxVariants}
         whileHover="hover"
+        whileDrag="drag"
         whileTap="click"
       />
     </Wrapper>
