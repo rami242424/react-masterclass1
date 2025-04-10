0f0e495338683cd35e18ef72ecc04af185470c68
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-11 08:01:59 +0900

    #9.5 Home Screen part one

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { createGlobalStyle } from "styled-components";
 
@@ -72,13 +73,17 @@ a {
 }
 `;
 
+const client = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
+    <QueryClientProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
+    </QueryClientProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
