4f95628f0a6f1609b807936551318396493f01a8
Rami <138556019+rami242424@users.noreply.github.com>
2025-04-10 15:43:42 +0900

    8.14 uoi need to watch this

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
+
 `;
 
 const Box = styled(motion.div)`
@@ -16,9 +16,12 @@ const Box = styled(motion.div)`
   height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+
+
   display: flex;
-  justify-content: flex-start;
-  align-items:flex-start;
+  justify-content: center;
+  align-items: center;
+
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -26,25 +29,42 @@ const Circle = styled(motion.div)`
   background-color: #00a5ff;
   height: 100px;
   width: 100px;
-  border-radius: 50px;
+
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-function App(){
+
+
+
+
+
+
+
+
+
+function App() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => {
-    setClicked((prev) => !prev);
-  }
+  const toggleClicked = () => setClicked((prev) => !prev);
+
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box 
-        style={{ 
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start"
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
       </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
+      </Box>
+
+
+
+
+
+
+
     </Wrapper>
   );
 }
