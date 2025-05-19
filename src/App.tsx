44b958e0cfb93de4941f5ae666cb2866cea8acf3
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-19 10:30:01 +0900

    8.4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion  } from "framer-motion";
+
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -12,15 +13,60 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255,255,255,0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
 `;
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 70px;
+  width: 70px;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  place-self: center;
+`;
+
+const boxVar = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition : {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    }
+  },
+}
+
+const circleVar = {
+  start:{
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  }
+}
 
 function App() {
   return (
     <Wrapper>
-      <Box/>
+      <Box variants={boxVar} initial="start" animate="end">
+        <Circle variants={circleVar}/>
+        <Circle variants={circleVar}/>
+        <Circle variants={circleVar}/>
+        <Circle variants={circleVar}/>
+      </Box>
     </Wrapper>
   );
 }
