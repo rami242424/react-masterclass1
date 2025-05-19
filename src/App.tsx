05ed9936aeca7999b8d88c5b10ac411794118a77
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-19 10:54:16 +0900

    8.5 gestures part one

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,30 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-
 `;
 
 const boxVar = {
   hover: {scale : 1.5, rotateZ : 90 },
-  click: { scale : 1, borderRadius: "100px"}
+  click: { scale : 1, borderRadius: "100px"},
+  drag: {
+    backgroundColor: "rgb(46,204,113)", 
+    transition: {duration : 3},
+  },
 }
 
 
 function App() {
   return (
     <Wrapper>
-      <Box 
+      <Box
+        drag
         variants={boxVar}
         whileHover="hover"
         whileTap="click"
+        whileDrag="drag"
       />
     </Wrapper>
   );
