96141b3249caf61a23d9701474f67ddbebdcda3f
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-19 15:16:06 +0900

    8.12nextPlz

--- src/App.tsx
+++ src/App.tsx
@@ -8,46 +8,78 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
+  width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 26px;
+  position: absolute;
+  top: 100px;
 `;
 
-const BoxVars = {
-  initial : {
+const BoxVar = {
+  invisible : {
+    x: 500,
     opacity: 0,
     scale: 0,
-  }, 
+  },
   visible : {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ : 360,
+    transition : {
+      duration: 1,
+    },
   },
   exit: {
+    x: -500,
     opacity: 0,
-    scale: 0,
-    y: 50,
+    // scale: 0,
+    rotateX : 180,
+    transition : {
+      duration: 1,
+    }
   },
 }
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggelShowing = () => {
-    setShowing((prev) => !prev)
+  const [visible, setVisible] = useState(1);
+  const nextPlz = () => {
+    setVisible((curr) => (curr === 10 ? 10 : curr + 1))
+  }
+  const prevPlz = () => {
+    setVisible((curr) => (curr === 1 ? 1 : curr - 1))
   }
   return (
     <Wrapper>
-      <button onClick={toggelShowing}>Click</button>
       <AnimatePresence>
-        {showing? <Box variants={BoxVars} initial="initial" animate="visible" exit="exit" /> : null}
+        {[1,2,3,4,5,6,7,8,9,10].map((n) => (
+            n === visible ? (
+              <Box 
+                key={n}
+                variants={BoxVar}
+                initial="invisible"
+                animate="visible"
+                exit="exit"
+              >
+                {n}
+              </Box>
+            ) : (
+              null
+            )
+          ))}
       </AnimatePresence>
+      <button onClick={nextPlz}>next</button>
+      <button onClick={prevPlz}>prev</button>
     </Wrapper>
   );
 }
