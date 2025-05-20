5ebe31b62d98a0e76612399de28ba582caa04098
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-20 12:20:19 +0900

    8.11 AnimatePresence : 일반 reactJs에도 animate쓸 수 있게 만듬

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+
 `;
 
 const Overlay = styled(motion.div)`
@@ -47,6 +48,22 @@ const Overlay = styled(motion.div)`
 //   visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
 //   exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 // };
+const BoxVar = {
+  initial : {
+    opacity: 0,
+    scale: 0,
+  },
+  visible : {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    y:20,
+    scale: 0,
+  },
+}
 
 function App() {
   const [showing, setShowing] = useState(false);
@@ -57,7 +74,19 @@ function App() {
   return (
     <Wrapper>
       <button onClick={onChange}>Click</button>
-      { showing? <Box /> : null }
+      <AnimatePresence> 
+        { showing? (
+          <Box 
+            variants={BoxVar}
+            initial="initial"
+            animate="visible"
+            exit="leaving"
+          />
+          ) : (
+          null
+          )
+        } 
+      </AnimatePresence>
     </Wrapper>
   );
 }
