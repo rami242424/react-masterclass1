3e8cfd6910a1d8e337f41ba39aff7888b70f2338
Rami <138556019+rami242424@users.noreply.github.com>
2025-05-20 12:07:27 +0900

    8.10 fill,pathLength,transition:default 로 나눠서 animate가능

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence, useMotionValue, useTransform, useViewportScrol
 import { useEffect, useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 300vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   justify-content: space-around;
@@ -47,30 +47,47 @@ const Overlay = styled(motion.div)`
 //   exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
 // };
 
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  stroke: white;
+  stroke-width: 2;
+`;
 
+const SvgVar = {
+  start: {
+    fill: "rgba(255,255,255,0)",
+    pathLength: 0,
+  },
+  end: {
+    fill: "rgba(255,255,255,1)",
+    pathLength: 1,
+  }
+}
 function App() {
-  const x = useMotionValue(0);
-  const {scrollY, scrollYProgress} = useViewportScroll();
-  useEffect(() => {
-    scrollY.onChange(() => console.log(scrollY.get(), scrollYProgress.get()));
-  }, [scrollY, scrollYProgress])
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
-  const WrapperGradient = useTransform(
-      x, 
-      [-800, 800],
-      [
-      "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
-      "linear-gradient(135deg, rgb(211, 94, 17), rgb(228, 85, 85))"
-      ],
-    );
-  const scale = useTransform(scrollYProgress, [0, 1], [0.1, 5]);
+  
   return (
-    <Wrapper style={{background:WrapperGradient}}>
-      <Box 
-        drag="x"
-        dragSnapToOrigin
-        style={{ x, rotateZ, scale}}
-      />
+    <Wrapper>
+      <Svg
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 448 512"
+      >
+        <motion.path
+          variants={SvgVar}
+          initial="start"
+          animate="end"
+          transition={{
+            default : {
+              duration: 5,
+            },
+            fill: {
+              duration : 2,
+              delay: 5,
+            }
+          }}
+          d="M224 373.1c-25.2-31.7-40.1-59.4-45-83.2-22.6-88 112.6-88 90.1 0-5.5 24.3-20.3 52-45 83.2zm138.2 73.2c-42.1 18.3-83.7-10.9-119.3-50.5 103.9-130.1 46.1-200-18.9-200-54.9 0-85.2 46.5-73.3 100.5 6.9 29.2 25.2 62.4 54.4 99.5-32.5 36.1-60.6 52.7-85.2 54.9-50 7.4-89.1-41.1-71.3-91.1 15.1-39.2 111.7-231.2 115.9-241.6 15.8-30.1 25.6-57.4 59.4-57.4 32.3 0 43.4 25.9 60.4 59.9 36 70.6 89.4 177.5 114.8 239.1 13.2 33.1-1.4 71.3-37 86.6zm47-136.1C280.3 35.9 273.1 32 224 32c-45.5 0-64.9 31.7-84.7 72.8C33.2 317.1 22.9 347.2 22 349.8-3.2 419.1 48.7 480 111.6 480c21.7 0 60.6-6.1 112.4-62.4 58.7 63.8 101.3 62.4 112.4 62.4 62.9 .1 114.9-60.9 89.6-130.2 0-3.9-16.8-38.9-16.8-39.6z"
+        ></motion.path>
+      </Svg>
     </Wrapper>
   );
 }
